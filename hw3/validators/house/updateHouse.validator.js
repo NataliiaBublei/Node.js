58a493f6f10994b77697e64459e78803c91750be
Natalya119 <natashabublej@gmail.com>
2019-10-12 21:35:54 +0300

    fixed

--- hw3/validators/house/updateHouse.validator.js
+++ hw3/validators/house/updateHouse.validator.js
@@ -2,7 +2,7 @@ module.exports = houseObject => {
 
     const {square, city, street, price} = houseObject;
 
-    if (!square || !city || !street || !price) {
+    if (!+square || !city || !street || !+price) {
         throw new Error('House is not valid')
     }
 };
