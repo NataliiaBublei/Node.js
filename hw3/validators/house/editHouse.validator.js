aa955db8e5631150282fb254e003b90a21077214
Natalya119 <natashabublej@gmail.com>
2019-10-12 18:09:51 +0300

    fixed

--- hw3/validators/house/editHouse.validator.js
+++
@@ -1,8 +0,0 @@
-module.exports = houseObject => {
-
-    const {id, square, city, street, price} = houseObject;
-
-    if (!id || !square || !city || !street || !price) {
-        throw new Error('House is not valid')
-    }
-};
--- hw3/validators/user/editUser.validator.js
+++
@@ -1,7 +0,0 @@
-module.exports = userObject => {
-    const {id, email, name, password} = userObject;
-
-    if (!id || !email || !name || !password) {
-        throw new Error('User is not valid')
-    }
-};
